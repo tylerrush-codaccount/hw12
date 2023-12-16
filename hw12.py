3997eb5308a8ef571d121139977bfd845c3af285
Tyler Rush <rusht504@dupage.edu>
2023-12-15 23:41:37 -0600

    Test
    Test

---
+++ hw12.py
@@ -0,0 +1,14 @@
+import sqlite3
+# Open the books database
+conn = sqlite3.connect('books.db')
+# Create a cursor
+cursor = conn.cursor()
+# Execute a query to select all data from the titles table
+cursor.execute("SELECT * FROM titles")
+# Get the metadata from the description attribute
+description = cursor.description
+# Get the data from the fetchall method
+data = cursor.fetchall()
+# Print the results in a tabular format
+for row in data:
+print("%-20s%-20s%-20s" % (row[0], row[1], row[2]))
